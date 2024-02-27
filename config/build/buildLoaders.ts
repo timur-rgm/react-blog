a8036fd0c717ff5b16bc73d72cbf660a0cf32a92
Timur <tiabumuaz@gmail.com>
2024-02-27 21:22:24 +0500

    feat: add babel & extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -39,5 +39,25 @@ export function buildLoaders(isDev: boolean): webpack.RuleSetRule[] {
         ]
     }
 
-    return [typescriptLoader, cssLoader, svgLoader, fileLoader]
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        'i18next-extract',
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        }
+                    ]
+                ]
+            }
+        }
+    }
+
+    return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader]
 }
