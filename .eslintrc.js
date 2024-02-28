f87f5390cd5e2b7eb441831904197b65e0577ccc
Timur <tiabumuaz@gmail.com>
2024-02-28 13:21:50 +0500

    feat: add eslint plugin for i18next

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
         ecmaVersion: 'latest',
         sourceType: 'module'
     },
-    plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+    plugins: ['@typescript-eslint', 'react', 'react-hooks', 'i18next'],
     rules: {
         'react/react-in-jsx-scope': 0,
         '@typescript-eslint/no-var-requires': 0,
@@ -29,5 +29,6 @@ module.exports = {
         '@typescript-eslint/no-unused-vars': 1,
         'react/button-has-type': 1,
         'react/jsx-sort-props': 1,
+        'i18next/no-literal-string': 1
     }
 }
