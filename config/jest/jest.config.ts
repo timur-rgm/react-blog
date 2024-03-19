c7cc22ac5e929ee3eff35b264fe7fc92a5585d58
Timur <tiabumuaz@gmail.com>
2024-03-19 22:07:44 +0500

    fix: add modulePaths to jest config

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -21,6 +21,7 @@ const config: Config = {
     coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
     moduleDirectories: ['node_modules'],
     moduleFileExtensions: ['js', 'mjs', 'cjs', 'jsx', 'ts', 'tsx', 'json', 'node'],
+    modulePaths: ['<rootDir>src'],
     testMatch: ['<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'],
     rootDir: '../../'
 
