50a1eb0f927b3a1871028529a485b7c0587a9515
Timur <tiabumuaz@gmail.com>
2024-04-04 23:00:02 +0500

    chore: write stories for Loader

---
+++ src/shared/ui/Loader/Loader.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator'
+import { Theme } from 'app/providers/ThemeProvider'
+import { Loader } from './Loader'
+
+const meta: Meta<typeof Loader> = {
+    component: Loader,
+}
+
+export default meta
+
+type Story = StoryObj<typeof Loader>
+
+export const Light: Story = {
+    args: {}
+}
+
+export const Dark: Story = {
+    args: {},
+    decorators: [(Story) => ThemeDecorator(Theme.DARK, Story)]
+}
