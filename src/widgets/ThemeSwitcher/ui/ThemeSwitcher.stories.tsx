a384716f98c9d5e747f2678fdd707b36c6d7f5d2
Timur <tiabumuaz@gmail.com>
2024-04-04 23:02:33 +0500

    chore: write stories for ThemeSwitcher

---
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator'
+import { Theme } from 'app/providers/ThemeProvider'
+import { ThemeSwitcher } from './ThemeSwitcher'
+
+const meta: Meta<typeof ThemeSwitcher> = {
+    component: ThemeSwitcher,
+}
+
+export default meta
+
+type Story = StoryObj<typeof ThemeSwitcher>
+
+export const Light: Story = {
+    args: {}
+}
+
+export const Dark: Story = {
+    args: {},
+    decorators: [(Story) => ThemeDecorator(Theme.DARK, Story)]
+}
