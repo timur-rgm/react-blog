9dd8495cf5515b2cb5da3d8cf15e6586df971f9a
Timur <tiabumuaz@gmail.com>
2024-04-28 08:56:50 +0400

    feat: add sizes, radius & variants for Button component

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -12,31 +12,73 @@ export default meta
 
 type Story = StoryObj<typeof Button>
 
-export const Default: Story = {
+export const VariantDefault: Story = {
     args: {
         children: 'Text',
         variant: 'default'
     }
 }
 
-export const Clear: Story = {
+export const VariantClear: Story = {
     args: {
         children: 'Text',
         variant: 'clear'
     }
 }
 
-export const Outline: Story = {
+export const VariantOutline: Story = {
     args: {
         children: 'Text',
         variant: 'outline'
     }
 }
 
-export const OutlineDark: Story = {
+export const VariantOutlineDark: Story = {
     args: {
         children: 'Text',
         variant: 'outline'
     },
     decorators: [(Story) => ThemeDecorator(Theme.DARK, Story)]
+}
+
+export const VariantBackground: Story = {
+    args: {
+        children: 'Text',
+        variant: 'background'
+    }
+}
+
+export const VariantBackgroundInverted: Story = {
+    args: {
+        children: 'Text',
+        variant: 'backgroundInverted'
+    }
+}
+
+export const RadiusNone: Story = {
+    args: {
+        children: 'Text',
+        radius: 'none'
+    }
+}
+
+export const RadiusSmall: Story = {
+    args: {
+        children: 'Text',
+        radius: 'small'
+    }
+}
+
+export const SizeSmall: Story = {
+    args: {
+        children: 'Text',
+        size: 'small'
+    }
+}
+
+export const SizeMedium: Story = {
+    args: {
+        children: 'Text',
+        size: 'medium'
+    }
 }
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -2,17 +2,29 @@ import { ReactNode, ButtonHTMLAttributes } from 'react'
 import cn from 'classnames'
 import styles from './Button.module.scss'
 
-type Variant = 'default' | 'clear' | 'outline'
+type Radius = 'none' | 'small'
+type Size = 'small' | 'medium' | 'big'
+type Variant = 'default' | 'clear' | 'outline' | 'background' | 'background-inverted'
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode
     className?: string
+    radius?: Radius
+    size?: Size
     variant?: Variant
 }
 
-export const Button = ({ children, className,  variant = 'default', ...otherProps }: Props) => {
+export const Button = ({ children, className, radius = 'none', size = 'small', variant = 'default', ...otherProps }: Props) => {
+    const rootClasses = cn(
+        styles.root,
+        styles[`radius-${radius}`],
+        styles[`size-${size}`],
+        styles[`variant-${variant}`],
+        className
+    )
+
     return (
-        <button className={cn(styles.root, styles[variant], className)} {...otherProps}>
+        <button className={rootClasses} {...otherProps}>
             {children}
         </button>
     )
