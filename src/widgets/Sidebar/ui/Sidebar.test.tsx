0caf03955a90a76e5776a06a742db1ce055ee6de
Timur <66139449+timur-rgm@users.noreply.github.com>
2024-04-29 10:30:24 +0400

    feat: add menu to sidebar
    * feat: style Sidebar switchers
    
    * feat: style Sidebar switchers
    
    * feat: style Sidebar Nav menu
    
    * fix tests

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,15 +1,15 @@
 import { screen, fireEvent } from '@testing-library/react'
 import { Sidebar } from 'widgets/Sidebar'
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation'
+import { renderComponent } from "shared/lib/tests/renderComponent/renderComponent";
 
 describe('Sidebar', () => {
     test('Test render', () => {
-        renderWithTranslation(<Sidebar />)
+        renderComponent(<Sidebar />)
         expect(screen.getByTestId('sidebar')).toBeInTheDocument()
     })
 
     test('Test toggle', () => {
-        renderWithTranslation(<Sidebar />)
+        renderComponent(<Sidebar />)
         const toggleButton = screen.getByTestId('sidebar-button')
         fireEvent.click(toggleButton)
         expect(screen.getByTestId('sidebar')).toHaveClass('collapsed')
