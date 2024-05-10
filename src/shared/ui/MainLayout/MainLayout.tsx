7e97eb050583e29bda37325e274e06c99315a90c
Timur <tiabumuaz@gmail.com>
2024-05-10 12:24:52 +0400

    feat: add Modal component

--- src/shared/ui/MainLayout/MainLayout.tsx
+++ src/shared/ui/MainLayout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import cn from 'classnames'
 
@@ -10,17 +10,32 @@ import { PageLoader } from 'widgets/PageLoader'
 
 import styles from './MainLayout.module.scss'
 
+import { Modal } from 'shared/ui/Modal/Modal'
+
 export const MainLayout = () => {
     const { theme } = useTheme()
+
+    const [isOpen, setIsOpen] = useState(false)
+    const handleToggle = () => {
+        setIsOpen((prev) => !prev)
+    }
 
     return (
         <div className={cn('app', theme)}>
             <header>
                 <Navbar />
             </header>
+            <button onClick={handleToggle}>Открыть</button>
 
             <main className={styles.main}>
                 <Sidebar />
+                <Modal isOpen={isOpen} onClose={handleToggle}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                    magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+                </Modal>
 
                 <div className={styles.content}>
                     <ErrorBoundary>
---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,51 @@
+import { ReactNode, MouseEvent, useEffect, useCallback } from 'react'
+import cn from 'classnames'
+import styles from './Modal.module.scss'
+
+interface Props {
+    children: ReactNode
+    className?: string
+    isOpen: boolean
+    onClose?: () => void
+}
+
+export const Modal = ({ children, className, isOpen, onClose }: Props) => {
+    const handleClose = useCallback(() => {
+        if (onClose) {
+            onClose()
+        }
+    }, [onClose])
+
+    const handleContentClick = (event: MouseEvent<HTMLDivElement>) => {
+        event.stopPropagation()
+    }
+
+    const handleEscKey = useCallback(
+        (event: KeyboardEvent) => {
+            if (event.key === 'Escape') {
+                handleClose()
+            }
+        },
+        [handleClose]
+    )
+
+    useEffect(() => {
+        if (isOpen) {
+            window.addEventListener('keydown', handleEscKey)
+        }
+
+        return () => {
+            removeEventListener('keydown', handleEscKey)
+        }
+    }, [isOpen, handleEscKey])
+
+    return (
+        <div className={cn(styles.root, className, { [styles.opened]: isOpen })}>
+            <div className={styles.overlay} onClick={handleClose}>
+                <div className={styles.content} onClick={handleContentClick}>
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
