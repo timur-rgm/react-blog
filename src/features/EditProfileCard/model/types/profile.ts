bd744f7156b6bad0de255f04abd1c1a2539d085f
Timur <tiabumuaz@gmail.com>
2024-09-14 14:59:55 +0500

    refactor: move state to EditProfileCard feature, add дщфloading & error statuses handling

---
+++ src/features/EditProfileCard/model/types/profile.ts
@@ -0,0 +1,19 @@
+import { Currency, Country } from 'shared/const/common'
+
+export interface Profile {
+    first: string
+    lastname: string
+    age: number
+    currency: Currency
+    country: Country
+    city: string
+    username: string
+    avatar: string
+}
+
+export interface ProfileState {
+    data?: Profile
+    isLoading: boolean
+    error?: string
+    readonly: boolean
+}
